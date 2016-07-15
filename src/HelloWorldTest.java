8473419b98e57c6d1d48f4cd8e6e145c7e47a03b
Raghavendra S <s_raghavendra@rocketmail.com>
2016-07-15 10:29:47 +0530

    adding again

---
+++ src/HelloWorldTest.java
@@ -0,0 +1,12 @@
+public class HelloWorldTest extends junit.framework.TestCase 
+{
+
+    public void testNothing() {}
+	    
+    public void testWillAlwaysFail() 
+	{
+        fail("An error message");
+    }
+			        
+}
+
--- src/raghu/HelloWorld.java
+++ src/raghu/HelloWorld.java
@@ -1,8 +1,13 @@
 package raghu;
 
-public class HelloWorld {
+import org.apache.log4j.Logger;
+import org.apache.log4j.BasicConfigurator;
 
-        public static void main(String[] args) {
-	    System.out.println("Hello World");
-	}
+public class HelloWorld {
+    static Logger logger = Logger.getLogger(HelloWorld.class);
+
+    public static void main(String[] args) {
+        BasicConfigurator.configure();
+        logger.info("Hello World");          // the old SysO-statement
+    }
 }
